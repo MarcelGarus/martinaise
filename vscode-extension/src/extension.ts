f62be1569d8ad201ba8b084c37c88adc50a54ae4
Marcel Garus <github@marcelgarus.dev>
2024-05-05 22:22:29 +0200

    Show diagnostics as you edit the code

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,22 +1,20 @@
 import * as child_process from "child_process";
-import { readFile } from "fs/promises";
-import * as stream from "stream";
+import * as linebyline from "linebyline";
 import * as vs from "vscode";
 import {
-  StreamInfo,
   integer
 } from "vscode-languageclient/node";
 
+let diagnosticCollection: vs.DiagnosticCollection;
+
 export async function activate(context: vs.ExtensionContext) {
   console.info("Activated Martinaise extension!");
-  loadCompiler();
 
   diagnosticCollection = vs.languages.createDiagnosticCollection("martinaise");
   context.subscriptions.push(diagnosticCollection);
 
-  vs.window.onDidChangeVisibleTextEditors(() => {
-    update();
-  });
+  vs.window.onDidChangeVisibleTextEditors(() => update());
+  vs.workspace.onDidChangeTextDocument((_: vs.TextDocumentChangeEvent) => update());
   // vs.workspace.onDidCloseTextDocument((document) => {
   //   // hints.delete(document.uri.toString());
   // });
@@ -41,32 +39,27 @@ export function deactivate(): Thenable<void> | undefined {
   return undefined;
 }
 
-// The Soil binary that is the compiler. It's only loaded once at the beginning
-// instead of every time we want to execute the compiler.
-let compiler: Buffer | null = null;
-async function loadCompiler() {
-  console.info("Loading compiler");
-  compiler = await readFile(`/home/marcel/projects/martinaise/martinaise.soil`);
-  console.log(`Loaded compiler (${compiler.byteLength} bytes)`);
-}
-
-let diagnosticCollection: vs.DiagnosticCollection;
-
 function update() {
+  console.log("Updating");
   for (const editor of vs.window.visibleTextEditors) {
     const uri = editor.document.uri.toString();
     if (!uri.endsWith(".mar")) continue;
 
     check(uri).then(errors => {
+      console.info("Displaying errors: " + errors);
       diagnosticCollection.clear();
       let diagnosticMap: Map<string, vs.Diagnostic[]> = new Map();
-      errors.forEach(error => {
-        let canonicalFile = vs.Uri.file(`${error.source.file}.mar`).toString();
-        let range = new vs.Range(1, 1, 1, 3);
-        let diagnostics = diagnosticMap.get(canonicalFile) ?? [];
-        diagnostics.push(new vs.Diagnostic(range, error.title, vs.DiagnosticSeverity.Error));
-        diagnosticMap.set(canonicalFile, diagnostics);
-      });
+      for (const error of errors) {
+        console.info("Error: " + JSON.stringify(error));
+        if (error.source.file != uri) continue;
+        let range = new vs.Range(
+          editor.document.positionAt(error.source.start),
+          editor.document.positionAt(error.source.end),
+        );
+        let diagnostics = diagnosticMap.get(error.source.file) ?? [];
+        diagnostics.push(new vs.Diagnostic(range, `${error.title}\n${error.description}`, vs.DiagnosticSeverity.Error));
+        diagnosticMap.set(error.source.file, diagnostics);
+      }
       diagnosticMap.forEach((diags, file) => {
         diagnosticCollection.set(vs.Uri.parse(file), diags);
       });
@@ -96,57 +89,66 @@ function workspaceFolder(): string | undefined {
   if (!folder) return undefined;
   return folder.uri.toString();
 }
-function streamToString(stream: stream.Readable): Promise<string> {
-  const chunks: any[] = [];
-  return new Promise((resolve, reject) => {
-    stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
-    stream.on('error', (err) => reject(err));
-    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
-  });
+
+/// Returns the source code of the given URI. Prefers the content of open text
+/// documents, even if they're not saved yet. If none exists, asks the file
+/// system.
+async function readSource(uri: vs.Uri): Promise<string | null> {
+  for (const doc of vs.workspace.textDocuments)
+    if (doc.uri.toString() == uri.toString()) return doc.getText(); else console.log("URIs don't match: " + doc.uri + " and " + uri);
+  try {
+    const bytes = await vs.workspace.fs.readFile(uri);
+    return new TextDecoder("utf8").decode(bytes);
+  } catch (e) {
+    return null;
+  }
 }
+
+// function streamToString(stream: stream.Readable): stream.Readable<string> {
+//   const chunks: any[] = [];
+//   return new Promise((resolve, reject) => {
+//     stream.on('data', (chunk) => {
+//       chunks.push(Buffer.from(chunk)))
+//     },
+//     stream.on('error', (err) => reject(err));
+//     stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+//   });
+// }
 async function check(path: string): Promise<MartinaiseError[]> {
-  // return [];
-  
-  console.info("Spawning /home/marcel/projects/soil/soil-asm");
-  // const ls = child_process.spawn("ls", ["-lh", "/usr"]);
-  // ls.stdout.on("data", (data) => {
-  //   console.log(`ls stdout: ${data}`);
-  // });
+  let soilBinary = "/home/marcel/projects/soil/soil-asm";
+  let martinaiseCompiler = "/home/marcel/projects/martinaise/martinaise.soil";
 
-  let soil = child_process.spawn(
-    "/home/marcel/projects/soil/soil-asm",
-    ["soil", path],
-    {
-      // "-json", "soil", path
-      // cwd: workspaceFolder(),
-      // shell: true,
-    },
-  );
-  soil.on("error", (data) => console.error(`Spawning soil failed: ${data}`));
-  soil.stdin.write(compiler);
-  soil.stdout.on("data", (data) => {
-    console.log(`soil stdout: ${data}`);
+  console.info(`Spawning ${soilBinary} with ${path}`);
+  let soil = child_process.spawn(soilBinary, [martinaiseCompiler, "analyze", path]);
+  soil.on("error", (error) => console.error(`Failed to spawn: ${error}`));
+  linebyline(soil.stderr).on("line", async function (line: string) {
+    console.log(line);
   });
-  soil.stderr.on("data", (data) => {
-    console.log(`soil stderr: ${data}`);
+
+  let diagnostics: MartinaiseError[] = [];
+  linebyline(soil.stdout).on('line', async function (line: string) {
+    console.info("Line: " + line);
+    const message = JSON.parse(line);
+    if (message.type == "read_file") {
+      const uri = vs.Uri.parse(message.path);
+      const content = await readSource(uri);
+      // console.info("Read file: " + content);
+      if (content) {
+        soil.stdin.write(JSON.stringify({ type: "read_file", success: true, content: content }));
+        soil.stdin.write("\n");
+      } else {
+        soil.stdin.write(JSON.stringify({ type: "read_file", success: false }));
+      }
+    }
+    if (message.type == "error") {
+      diagnostics.push(message as MartinaiseError);
+    }
   });
-  // console.log("Spawned");
-  // let compiler = await readFile(`${workspaceFolder()}/martinaise.soil`)
-  // console.log("Writing compiler");
-  // soil.stdin.write(compiler);
-  // console.log("Closing stdin");
-  // soil.stdin.destroy();
 
-  // console.log("Waiting for exit");
-  // await new Promise((resolve) => process.on("exit", () => resolve(null)));
-  // let exitCode = process.exitCode;
-  // console.info("Martinaise exited with " + exitCode);
-  // let output = await streamToString(process.stderr);
-  // console.info(output);
+  const exitCode = await new Promise((resolve) => soil.on("close", (exitCode) => resolve(exitCode)));
+  console.info(`Martinaise exited with ${exitCode}.`);
 
-  // let diagnostics = output.split("\n").map((line) => JSON.parse(line));
-  // console.error(diagnostics);
-  return [];
+  return diagnostics;
 }
 type MartinaiseError = {
   source: {
@@ -191,104 +193,5 @@ class MartinaiseCodeActionsProvider implements vs.CodeActionProvider {
   }
   resolveCodeAction?(codeAction: vs.CodeAction, _: vs.CancellationToken): vs.ProviderResult<vs.CodeAction> {
     return codeAction;
-  }
-}
-
-// The following code is taken (and slightly modified) from https://github.com/Dart-Code/Dart-Code
-function spawnServer(): Promise<StreamInfo> {
-  const process = safeSpawn();
-  console.info(`PID: ${process.pid}`);
-
-  let reader = process.stdout;
-  let writer = process.stdin;
-
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  if (enableLogging) {
-    reader = process.stdout.pipe(new LoggingTransform("<=="));
-    writer = new LoggingTransform("==>");
-    writer.pipe(process.stdin);
-  }
-
-  process.stderr.on("data", (data) => {
-    console.error(String(data));
-  });
-
-  process.addListener("close", (exitCode) => {
-    if (exitCode === 101) {
-      console.error("LSP server was closed with a panic.");
-    } else {
-      console.error(`LSP server was closed with code ${exitCode}.`);
-    }
-  });
-  process.addListener("disconnect", () => {
-    console.error("LSP server disconnected.");
-  });
-  process.addListener("error", (event) => {
-    console.error(`LSP server had an error: ${event.toString()}`);
-  });
-  process.addListener("exit", (exitCode) => {
-    if (exitCode === 101) {
-      console.error("LSP server panicked.");
-    } else {
-      console.error(`LSP server exited with exit code ${exitCode}.`);
-    }
-  });
-  process.addListener("message", () => {
-    console.error("LSP server sent a message.");
-  });
-
-  return Promise.resolve({ reader, writer });
-}
-
-type SpawnedProcess = child_process.ChildProcess & {
-  stdin: stream.Writable;
-  stdout: stream.Readable;
-  stderr: stream.Readable;
-};
-function safeSpawn(): SpawnedProcess {
-  const configuration = vs.workspace.getConfiguration("candy");
-
-  let command: [string, string[]] = ["candy", ["lsp"]];
-  const languageServerCommand = configuration.get<string>(
-    "languageServerCommand",
-  );
-  if (languageServerCommand && languageServerCommand.trim().length !== 0) {
-    const parts = languageServerCommand.split(" ");
-    command = [parts[0], parts.slice(1)];
-  }
-
-  return child_process.spawn(command[0], command[1], {
-    cwd: vs.workspace.rootPath,
-    env: { ...process.env, RUST_BACKTRACE: "FULL" },
-    shell: true,
-  }) as SpawnedProcess;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-class LoggingTransform extends stream.Transform {
-  constructor(
-    private readonly prefix: string,
-    opts?: stream.TransformOptions,
-  ) {
-    super(opts);
-  }
-  public _transform(
-    chunk: unknown,
-    encoding: BufferEncoding,
-    callback: () => void,
-  ): void {
-    const value = (chunk as Buffer).toString();
-    const toLog = value
-      .split("\r\n")
-      .filter(
-        (line) => line.trim().startsWith("{") || line.trim().startsWith("#"),
-      )
-      .join("\r\n");
-    if (toLog.length > 0) {
-      console.info(`${this.prefix} ${toLog}`);
-    }
-
-    this.push(Buffer.from(value, "utf8"), encoding);
-    callback();
   }
 }
